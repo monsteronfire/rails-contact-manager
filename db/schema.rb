285ad2ab5f5a403649ed62c29a1d76cad659384b
monsteronfire <monsteronfire@gmail.com>
2017-06-05 21:31:54 +0800

    Add company scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170604084251) do
+ActiveRecord::Schema.define(version: 20170605133041) do
+
+  create_table "companies", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "email_addresses", force: :cascade do |t|
     t.string   "address"
