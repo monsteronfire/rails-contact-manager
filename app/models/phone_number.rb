223b2f16ea50fbeacaf30fb66d18ad493345c70c
monsteronfire <monsteronfire@gmail.com>
2017-06-07 10:51:06 +0800

    Fix controller specs for phone_number

--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -1,4 +1,4 @@
 class PhoneNumber < ApplicationRecord
   validates :contact_id, :number,  presence: true
-  belongs_to :contact, polymorphic: true
+  belongs_to :contact, polymorphic: true, optional: true
 end
