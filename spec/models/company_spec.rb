e3019c76a49e0b651649acda5717b6d0918d89d7
Shaii Ong <monsteronfire@gmail.com>
2017-06-10 00:01:44 +0800

    Update and fix broken company spec

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -14,7 +14,9 @@ RSpec.describe Company, type: :model do
     expect(company).not_to be_valid
   end
 
-  it 'has an array of phone number' do
-    expect(company.phone_numbers).to eq([])
+  it 'responds with its phone numbers after they are created' do
+    phone_number = company.phone_numbers.build(number: '333-4444');
+    expect(phone_number.number).to eq('333-4444')
   end
+
 end
