d1814faf877b001f72107aa3d0b2b4ebd260a1e9
Shaii Ong <monsteronfire@gmail.com>
2017-06-19 22:19:32 +0800

    Configure twitter auth

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   resources :phone_numbers, except: [:index, :show]
   resources :people
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/auth/:provider/callback' => 'sessions#create'
 end
