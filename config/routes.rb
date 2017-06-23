4785727656da333b094e5d04a2630e216bc123fc
Shaii Ong <monsteronfire@gmail.com>
2017-06-23 14:53:19 +0800

    Set root path

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :sessions, only: [:create]
   get '/auth/:provider/callback' => 'sessions#create'
+
+  root to: 'site#index'
 end
